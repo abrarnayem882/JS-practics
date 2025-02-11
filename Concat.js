62d396f602963ed127eb25ecfd28e9879d9154a0
MD.ABU NAYEM <rajnayem882@gmail.com>
2025-02-11 17:46:00 +0600

    runing practics

---
+++ Concat.js
@@ -0,0 +1,11 @@
+const first = 'Abrar';
+const last = 'Nayem';
+
+const fullname = first +' ' +last ;
+// console.log(fullname);
+
+const Fullname = first.concat(' ').concat(last);
+console.log(Fullname);
+
+//includes
+console.log(last.includes('W'))
---
+++ slice.js
@@ -0,0 +1,15 @@
+const address='Seedstore'
+const part=address.slice(2,5);
+console.log(part);
+
+const sentence = 'I am a good and hardworking person'
+console.log(sentence.split('a'));
+
+
+const friend='Rahim , Kahim ,Fahim ,lahim, Gahim, Lohim'
+const f=friend.split(',');
+console.log(friend);
+
+const realfriend = [ 'I ', 'm ', ' good ', 'nd h', 'rdworking person' ];
+console.log(realfriend);
+console.log(realfriend.join());
